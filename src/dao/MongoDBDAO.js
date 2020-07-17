54e8d5e85dc0b2ae64efe367be255d6f4a4a29f4
Sam <sam.to@ewoosoft.com>
2020-07-17 09:55:27 +0700

    modify filter condition

--- src/dao/MongoDBDAO.js
+++ src/dao/MongoDBDAO.js
@@ -1,5 +1,6 @@
 'use strict';
 const lodash = require('lodash');
+const { ObjectId } = require('mongodb');
 const MongoDBConnector = require('./../connectors/MongoDBConnector');
 
 class MongoDBDAO {
@@ -15,9 +16,10 @@ class MongoDBDAO {
         let dbConn = await MongoDBConnector.getConnection(this.databaseName,this.host,this.user,this.password);
         let collection = dbConn.collection(this.tableName);
         let bulkWriteReqArray = lodash.map(items, (item) => {
+            item['_id'] = item['_id'] ? item['_id'] : ObjectId().toString();
             return {
                 updateOne: {
-                    filter: { _id: item['_id'] },
+                    filter: { _id: ObjectId(item['_id']) },
                     update: {
                         $set: lodash.omit(item, '_id')
                     },
